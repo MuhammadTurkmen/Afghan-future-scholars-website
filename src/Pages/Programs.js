d25b92859ca9bda48ffa21cfcbd15071e23bc6be
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 14:26:54 +0430

    adding styled

--- src/Pages/Programs.js
+++ src/Pages/Programs.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 function Programs() {
   return <div>Programs</div>;
 }
+
+const Wrapper = styled.div`
+  background-color: white;
+`;
 
 export default Programs;
