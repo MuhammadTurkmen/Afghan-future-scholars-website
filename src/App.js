1974cc9b1ee5df997b4ca93176c13cbb9c392be3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 23:32:14 +0430

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Programs from "./Pages/Programs";
+import Involved from "./Pages/Involved";
+import Contact from "./Pages/Contact";
+import Footer from "./Components/Footer";
 import Error from "./Pages/Error";
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/involved" element={<Involved />} />
+          <Route path="/Contact" element={<Contact />} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
---
+++ src/Pages/Involved.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Involved() {
+  return <div>Involved</div>;
+}
+
+export default Involved;
