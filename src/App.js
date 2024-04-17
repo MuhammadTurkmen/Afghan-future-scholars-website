6fa0b6011cc06c7659380ad20d5938933e9f7deb
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 18:26:28 +0430

    adding react reouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Error from "./Pages/Error";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
