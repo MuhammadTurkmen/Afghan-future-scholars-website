3681f81667de39b74db666554b04612f1ff1e97d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 23:21:40 +0430

    finishing footer,js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,99 @@
 import React from "react";
+import styled from "styled-components";
+import { socials, reachUs } from "../Data";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="first-container">
+          <h1 className="title">Afghanistan Future Scholars</h1>
+          <p className="sub-title">
+            Empowering Afghan Women through <br /> Education. Support
+            Afghanistan Future <br /> Scholars in Building a Brighter Future.
+          </p>
+        </div>
+        <div className="second-container">
+          <h1>Social</h1>
+          <div className="social-container">
+            {socials.map(({ text, icon }, index) => {
+              return (
+                <div className="socials" key={index}>
+                  {icon}
+                  <p>{text}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="third-container">
+          <h1>Reach Us</h1>
+          <div className="reach-container">
+            {reachUs.map(({ icon, text }, index) => {
+              return (
+                <div className="reach" key={index}>
+                  {icon}
+                  <p>{text}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="black-container">
+        <p>All Rights Reserved 2024. Afghanistan Future Scholars</p>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  background-color: #131212;
+  color: white;
+
+  .container {
+    max-width: 1300px;
+    margin: 0 auto;
+    padding: 8rem 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+  }
+
+  .sub-title {
+    font-size: 18px;
+    line-height: 40px;
+    margin-top: 1rem;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 24px;
+    margin-bottom: 1.5rem;
+  }
+
+  .socials,
+  .reach {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    font-size: 30px;
+
+    p {
+      font-size: 20px;
+    }
+  }
+
+  .black-container {
+    text-align: center;
+    padding: 2.5rem;
+    background-color: black;
+    color: white;
+
+    p {
+      font-size: 12px;
+    }
+  }
+`;
 
 export default Footer;
--- src/Data.js
+++ src/Data.js
@@ -1,4 +1,14 @@
 import usingComputer from "./Images/using-computer.jpeg";
+import book from "./Images/book.jpeg";
+import coffee from "./Images/coffee-with-book.jpeg";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+//
+import { FaWhatsapp } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdLocalPostOffice } from "react-icons/md";
 
 export const links = [
   {
@@ -30,13 +40,47 @@ export const milestones_cards = [
     img: usingComputer,
   },
   {
-    title: "TOEFL Class",
+    title: "Computer Class",
     desc: "Our tailored programs help students achieve their best scores, enhancing their chances of securing scholarships and opportunities abroad",
-    img: usingComputer,
+    img: book,
   },
   {
     title: "TOEFL Class",
     desc: "Our tailored programs help students achieve their best scores, enhancing their chances of securing scholarships and opportunities abroad",
-    img: usingComputer,
+    img: coffee,
+  },
+];
+
+export const socials = [
+  {
+    icon: <FaLinkedin />,
+    text: "LinkedIn",
+  },
+  {
+    icon: <FaXTwitter />,
+    text: "Twitter",
+  },
+  {
+    icon: <FaFacebook />,
+    text: "Facebook",
+  },
+  {
+    icon: <FaInstagram />,
+    text: "Instagram",
+  },
+];
+
+export const reachUs = [
+  {
+    icon: <FaWhatsapp />,
+    text: "+93788275519",
+  },
+  {
+    icon: <FaPhoneAlt />,
+    text: "+93788275519",
+  },
+  {
+    icon: <MdLocalPostOffice />,
+    text: "Afg.future.scholars.org@gmail.com",
   },
 ];
